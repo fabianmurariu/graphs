9f1a007e38c717357c5ff62ecf69637359c3a8b1
Fabian Murariu <murariu.fabian@gmail.com>
2022-09-03 19:12:10 +0100

    added more headers

--- core/src/main/scala/com/github/fabianmurariu/graphs/data/dg/immutable/ImmutableLookupTable.scala
+++ core/src/main/scala/com/github/fabianmurariu/graphs/data/dg/immutable/ImmutableLookupTable.scala
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.github.fabianmurariu.graphs.data.dg.immutable
 
 import com.github.fabianmurariu.graphs.data.dg.LookupTable
